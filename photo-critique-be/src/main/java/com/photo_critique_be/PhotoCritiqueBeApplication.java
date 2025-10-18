6027ad5b7f46f5cf7980b053fb2109b1ae4ca357
laingoctrang <laingoctrang23@gmail.com>
2025-10-18 14:42:31 +0700

    init be project structure

---
+++ photo-critique-be/src/main/java/com/photo_critique_be/PhotoCritiqueBeApplication.java
@@ -0,0 +1,13 @@
+package com.photo_critique_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoCritiqueBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoCritiqueBeApplication.class, args);
+	}
+
+}
