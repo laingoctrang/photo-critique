010535f1a9f3373c1317406b758d8eff2b80c7e6
laingoctrang <laingoctrang23@gmail.com>
2025-11-29 13:41:05 +0700

    fix(be): missing class ReactionTargetType

---
+++ photo-critique-be/src/main/java/com/photo_critique_be/enums/ReactionTargetType.java
@@ -0,0 +1,7 @@
+package com.photo_critique_be.enums;
+
+public enum ReactionTargetType {
+    POST,
+    COMMENT,
+    MESSAGE
+}
--- photo-critique-be/src/main/java/com/photo_critique_be/enums/XPEventType.java
+++
@@ -1,6 +0,0 @@
-package com.photo_critique_be.enums;
-
-public enum XPEventType {
-    POST_CREATED, COMMENT_HELPFUL, POST_LIKED, COMMENT_LIKED,
-    BADGE_EARNED, FOLLOW_GAINED, POST_TRENDING
-}
